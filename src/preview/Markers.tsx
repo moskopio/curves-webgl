92504d23f5bec2e470c1731cb526a32812a1887d
moskopio <moskowicz.piotr@gmail.com>
2024-07-23 01:07:03 +0100

    fix markers

--- src/preview/Markers.tsx
+++ src/preview/Markers.tsx
@@ -1,4 +1,4 @@
-import { Fragment, ReactElement, useCallback, useState, MouseEvent } from "react"
+import { MouseEvent, ReactElement, useCallback, useState } from "react"
 import { Point, PointIndex, Points } from "../types"
 import './Markers.css'
 
@@ -11,7 +11,7 @@ export function Markers(props: Props): ReactElement {
   const { points, onChange } = props
   const { p0, p1, p2, p3 } = points
   
-  const [selectedMarker, selectMarker] = useState(null)
+  const [selectedMarker, selectMarker] = useState<PointIndex | null>(null)
   
   const onPointChange = useCallback((id: PointIndex, point: Point) => {
     const newPoints = {...points}
@@ -19,41 +19,38 @@ export function Markers(props: Props): ReactElement {
     onChange(newPoints)
   }, [points, onChange])
   
+  const handleDrag = useCallback((event: MouseEvent) => {
+    if (selectedMarker) {
+      const x = event?.clientX
+      const y = event?.clientY
+      onPointChange(selectedMarker, [x,y])
+    }
+  }, [selectedMarker, onPointChange])
+  const handleDragEnd = useCallback(() => selectMarker(null), [selectMarker])
+  
   return (
-    <svg className="svg-marker" >
-    <Marker id="p0" point={p0} onChange={onPointChange} />
-    <Marker id="p1" point={p1} onChange={onPointChange} />
-    <Marker id="p2" point={p2} onChange={onPointChange} />
-    <Marker id="p3" point={p3} onChange={onPointChange} />
+    <svg className="svg-marker" onMouseMove={handleDrag} onMouseUp={handleDragEnd}>
+    <Marker id="p0" point={p0} selectMarker={selectMarker} />
+    <Marker id="p1" point={p1} selectMarker={selectMarker} />
+    <Marker id="p2" point={p2} selectMarker={selectMarker} />
+    <Marker id="p3" point={p3} selectMarker={selectMarker} />
   </svg>
   )
 }
 
 interface MarkerProps {
-  id:       PointIndex
-  point:    [number, number]
-  onChange: (id: PointIndex, point: Point) => void
+  id:     PointIndex
+  point:  [number, number]
+  selectMarker: (id: PointIndex | null) => void
 }
 
 function Marker(props: MarkerProps): ReactElement {
-  const { id, point, onChange } = props
-  
-  const [isDragged, setIsDragged] = useState(false)
-  const handleDragStart = useCallback(() => setIsDragged(true), [setIsDragged])
-  const handleDragEnd = useCallback(() => setIsDragged(false), [setIsDragged])
-  const handleDrag = useCallback((event: MouseEvent<SVGGeometryElement>) => {
-    if (!isDragged) return
-    const x = event?.clientX
-    const y = event?.clientY
-    onChange(id, [x,y])
-  }, [isDragged])
-  
+  const { id, point, selectMarker } = props
+  const handleDragStart = useCallback(() => selectMarker(id), [selectMarker])
+
   return (
       <circle id={id} 
         onMouseDown={handleDragStart} 
-        onMouseUp={handleDragEnd}
-        onMouseMove={handleDrag}
-        onMouseLeave={handleDragEnd}
         cx={point[0]} 
         cy={point[1]} 
       />
